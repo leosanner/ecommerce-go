ba3edac81d1cc6dc822badec7715fafcf90b5dfe
Leonardo Sanner <leosanner12@gmail.com>
2026-06-15 10:19:42 -0300

    .

--- cmd/main.go
+++ cmd/main.go
@@ -1,28 +1,44 @@
 package main
 
 import (
+	"context"
 	"log/slog"
 	"os"
 
 	"github.com/jackc/pgx/v5"
+	"github.com/joho/godotenv"
+	"github.com/leosanner/ecommerce-go/internal/env"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		slog.Error("Server failed to start", "error", err)
+		os.Exit(1)
+	}
+
+	ctx := context.Background()
+
 	cfg := config{
 		addr: ":8080",
 		db: dbConfig{
-			dsn: "host=localhost user=postgres password=postgres dbname=ecom sslmode=disabled",
+			dsn: env.GetString("GOOSE_DBSTRING", "host=localhost user=postgres password=postgres dbname=ecom sslmode=disable"),
 		},
-	}
-
-	conn, err := pgx.Connect(ctx, cfg.db.dsn)
-
-	api := application{
-		config: cfg,
 	}
 
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 	slog.SetDefault(logger)
+
+	conn, err := pgx.Connect(ctx, cfg.db.dsn)
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close(ctx)
+
+	logger.Info("connected to database", "dsn", cfg.db.dsn)
+
+	api := application{
+		config: cfg,
+	}
 
 	if err := api.run(api.mount()); err != nil {
 		slog.Error("Server failed to start", "error", err)
