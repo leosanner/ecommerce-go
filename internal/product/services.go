2a91420d2a0e546a0356d2f5ba66d6115773eb3b
Leonardo Sanner <leosanner12@gmail.com>
2026-06-15 09:32:03 -0300

    .

--- internal/product/services.go
+++ internal/product/services.go
@@ -1,4 +1,18 @@
 package product
 
+import "context"
+
 type Service interface {
+	ListProducts(context.Context) error
+}
+
+type svc struct {
+}
+
+func (s *svc) ListProducts(ctx context.Context) error {
+	return nil
+}
+
+func NewService() Service {
+	return &svc{}
 }
