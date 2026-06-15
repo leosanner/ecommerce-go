5b2c1290d474b58094924f2f73048e0427461869
Leonardo Sanner <leosanner12@gmail.com>
2026-06-15 10:08:21 -0300

    .

--- cmd/main.go
+++ cmd/main.go
@@ -3,12 +3,19 @@ package main
 import (
 	"log/slog"
 	"os"
+
+	"github.com/jackc/pgx/v5"
 )
 
 func main() {
 	cfg := config{
 		addr: ":8080",
+		db: dbConfig{
+			dsn: "host=localhost user=postgres password=postgres dbname=ecom sslmode=disabled",
+		},
 	}
+
+	conn, err := pgx.Connect(ctx, cfg.db.dsn)
 
 	api := application{
 		config: cfg,
