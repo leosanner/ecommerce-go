0bca9ffab3520b5bffe5f03afae1cae936aba853
Leonardo Sanner <leosanner12@gmail.com>
2026-06-15 09:08:17 -0300

    .

---
+++ internal/product/handlers.go
@@ -0,0 +1,17 @@
+package product
+
+import "net/http"
+
+type handler struct {
+	service Service
+}
+
+func NewHandler(s Service) *handler {
+	return &handler{
+		service: s,
+	}
+}
+
+func (h *handler) ListProducts(w http.ResponseWriter, r *http.Request) {
+
+}
